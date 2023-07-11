076733f30b548bb84877bc22465eeb55a7c4b69e
Kotovasia <kotovasia5120@gmail.com>
2023-07-11 22:46:09 +0300

    Init and stringSplit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module GoCutedCalc
+
+go 1.20
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
-    }
+func main() {
+	var input string
+	fmt.Print("Введите математическую операцию (+, -, *, /), разделяя операнды и оператор пробелами: ")
+	fmt.Scanln(&input)
+
+	// Разбиваем строку на операнды и оператор
+	parts := strings.Split(input, " ")
+	if len(parts) != 3 {
+		fmt.Println("Неверный формат операции")
+		return
+	}
+}
