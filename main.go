5e4287e29441a0e775a2b6fc034e83e49f0b021f
Kotovasia <kotovasia5120@gmail.com>
2023-07-13 22:33:49 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	var input string
+	in := bufio.NewReader(os.Stdin)
+	input, err := in.ReadString('\n')
+	if err != nil {
+		fmt.Println("Ошибка ввода: ", err)
+	}
 	fmt.Print("Введите математическую операцию (+, -, *, /), разделяя операнды и оператор пробелами: ")
-	fmt.Scanln(&input)
+
+	fmt.Println(input)
 
 	// Разбиваем строку на операнды и оператор
 	parts := strings.Split(input, " ")
@@ -16,4 +24,30 @@ func main() {
 		fmt.Println("Неверный формат операции")
 		return
 	}
+
+	// Преобразуем операнды в числа
+	num1, err1 := strconv.ParseFloat(parts[0], 64)
+	num2, err2 := strconv.ParseFloat(parts[2], 64)
+	if err1 != nil || err2 != nil {
+		fmt.Println("Неверный формат операндов")
+		return
+	}
+
+	// Выполняем операцию
+	var result float64
+	switch parts[1] {
+	case "+":
+		result = num1 + num2
+	case "-":
+		result = num1 - num2
+	case "*":
+		result = num1 * num2
+	case "/":
+		result = num1 / num2
+	default:
+		fmt.Println("Неверный оператор")
+		return
+	}
+
+	fmt.Printf("%.2f %s %.2f = %.2f", num1, parts[1], num2, result)
 }
